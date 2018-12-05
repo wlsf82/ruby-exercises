359ee04887f2530c156ab26735d917df1b33ac68
Walmyr <wlsf82@gmail.com>
2018-12-05 16:38:37 +0100

    Create simple calculator program

---
+++ calculator.rb
@@ -0,0 +1,29 @@
+class Calculator
+  def add(a, b)
+    a+b
+  end
+
+  def subtract(a, b)
+    a-b
+  end
+
+  def multiply(a, b)
+    a*b
+  end
+
+  def divide(a, b)
+    a/b
+  end
+end
+
+calculator = Calculator.new
+
+result_of_add = calculator.add(1, 2)
+result_of_subtract = calculator.subtract(10, 4)
+result_of_multiply = calculator.multiply(2, 5)
+result_of_divide = calculator.divide(4, 2)
+
+puts result_of_add
+puts result_of_subtract
+puts result_of_multiply
+puts result_of_divide
