f4fd46e7b0787f7fb282f65efe14dbe70a4f6bd6
Walmyr <wlsf82@gmail.com>
2018-12-09 23:40:46 +0100

    Refactor fizzbuzz class to use fizzbuzz engine

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,5 @@
+require_relative 'fizz_buzz_engine'
+
 class FizzBuzz
   def numbers
     (1..100).to_a
@@ -5,21 +7,8 @@ class FizzBuzz
 
   def sequence
     numbers.collect do |x|
-      if multiple_of(3, x) and multiple_of(5, x)
-        'Fizz Buzz'
-      elsif multiple_of(3, x)
-        'Fizz'
-      elsif multiple_of(5, x)
-        'Buzz'
-      else
-        x
-      end
+      fbe = FizzBuzzEngine.new(x)
+      fbe.value
     end
-  end
-
-  private
-
-  def multiple_of(n, x)
-    x % n == 0
   end
 end
