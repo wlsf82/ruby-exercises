92789ff5b99bbf18b8953d6ee3899c422adfc1bf
Walmyr <wlsf82@gmail.com>
2018-12-11 00:06:51 +0100

    Refactor stack class to handle edge cases

--- stack/stack.rb
+++ stack/stack.rb
@@ -3,11 +3,18 @@ class Stack
     @elements = []
   end
 
+  def empty?
+    @elements.empty?
+  end
+
   def push(element)
     @elements << element
   end
 
   def pop
+    if @elements.empty?
+      raise Exception.new('Cannot pop an empty stack')
+    end
     @elements.pop
   end
 
