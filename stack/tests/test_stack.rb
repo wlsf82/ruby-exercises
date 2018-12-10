8c74c3ebf797c89fbb9af81b73692531482837b8
Walmyr <wlsf82@gmail.com>
2018-12-11 00:06:23 +0100

    Implements tests to handle edge cases

--- stack/tests/test_stack.rb
+++ stack/tests/test_stack.rb
@@ -2,6 +2,50 @@ require 'minitest/autorun'
 require_relative '../stack'
 
 class TestStack < Minitest::Test
+  def test_stack_should_be_empty_on_construction
+    stack = Stack.new
+
+    assert stack.empty?
+  end
+
+  def test_after_n_pushes_to_an_empty_stack_the_stack_is_not_empty
+    stack = Stack.new
+
+    stack.push(2)
+    stack.push(4)
+
+    refute stack.empty?
+  end
+
+  def test_after_n_pushes_to_an_empty_stack_its_size_is_n
+    stack = Stack.new
+
+    stack.push(2)
+    stack.push(4)
+
+    assert_equal 2, stack.size
+  end
+
+  def test_stack_with_n_elements_becomes_empty_after_n_pops
+    stack = Stack.new
+
+    stack.push(2)
+    stack.push(4)
+
+    stack.pop
+    stack.pop
+
+    assert stack.empty?
+  end
+
+  def test_popping_from_an_empty_stack_throws_an_exception
+    stack = Stack.new
+
+    assert_raises(Exception) do
+      stack.pop
+    end
+  end
+
   def test_should_push_a_given_item
     stack = Stack.new
 
