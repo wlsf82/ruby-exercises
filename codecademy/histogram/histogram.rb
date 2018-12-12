21456f69ba137533f5c1a1da311cd9abb3edc6ae
Walmyr <wlsf82@gmail.com>
2018-12-12 15:57:17 +0100

    Exercising hashes, iteraction, sort_by, and reverse!

---
+++ codecademy/histogram/histogram.rb
@@ -0,0 +1,20 @@
+puts "Input please: "
+text = gets.chomp
+
+words = text.split(" ")
+
+frequencies = Hash.new(0)
+
+words.each do |word|
+  frequencies[word] += 1
+end
+
+frequencies = frequencies.sort_by do |word, count|
+  count
+end
+
+frequencies.reverse!
+
+frequencies.each do |word, count|
+  puts word + " " + count.to_s
+end
