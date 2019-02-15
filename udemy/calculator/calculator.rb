37812142b77d88046238a07d57211f57fcde985b
Walmyr Filho <limaesilva@qelp.com>
2019-02-15 15:21:57 +0100

    Remove commented code to make it cleaner and improve score on BCH

--- udemy/calculator/calculator.rb
+++ udemy/calculator/calculator.rb
@@ -15,15 +15,3 @@ class Calculator
     a/b
   end
 end
-
-# calculator = Calculator.new
-
-# result_of_add = calculator.add(1, 2)
-# result_of_subtract = calculator.subtract(10, 4)
-# result_of_multiply = calculator.multiply(2, 5)
-# result_of_divide = calculator.divide(4, 2)
-
-# puts "1 + 2 = #{result_of_add}"
-# puts "10 - 4 = #{result_of_subtract}"
-# puts "2 * 5 = #{result_of_multiply}"
-# puts "4 / 2 = #{result_of_divide}"
